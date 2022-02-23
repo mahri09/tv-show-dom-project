6ad80d47dce7eb5b4cfce6c58d9ef817456c690a
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-02-23 23:10:12 +0000

    completed Level-100

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  let result = createEpisodeCards(allEpisodes);
+  makePageForEpisodes(result);
 }
 
-function makePageForEpisodes(episodeList) {
+const createEpisodeCards = (episodesArr) => {
+  // Create card element
+  const card = document.createElement("div");
+  const twoDigit = (n) => (n < 10 ? "0" + n : n);
+  episodesArr.forEach((episode) => {
+    card.classList = "card-body";
+    // Construct card content
+    const content = `
+    <div index=${episode.id} class='card'>
+    <div class='card-image'><img src=${episode.image.medium} alt=card-image ${
+      episode.name
+    }></div>
+      <h2 class='card-title'> ${episode.name} - S${twoDigit(
+      episode.season
+    )}E${twoDigit(episode.number)} </h2>
+      ${episode.summary}
+    </div>
+      `;
+    // Append newly created card element to the card
+    // console.log(card);
+    card.innerHTML += content;
+  });
+  return card;
+};
+
+const makePageForEpisodes = (episodeList) => {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  rootElem.append(episodeList);
+};
 
 window.onload = setup;
