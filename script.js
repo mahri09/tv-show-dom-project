f54ddd29b8ae77c400af1e8200a0bc7a6ecf222b
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-02-24 12:57:56 +0000

    completed Level-200, added seacrh button, and displayed length of cards

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-//You can edit ALL of the code here
+const allEpisodes = getAllEpisodes();
+const cardBody = document.getElementById("card-body");
+const searchInput = document.getElementById("search-input");
+const displayLength = document.querySelector(".display-length");
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  let result = createEpisodeCards(allEpisodes);
-  makePageForEpisodes(result);
+  makePageForEpisodes(allEpisodes);
 }
 
-const createEpisodeCards = (episodesArr) => {
+const createEpisodeCards = (episode) => {
   // Create card element
-  const card = document.createElement("div");
   const twoDigit = (n) => (n < 10 ? "0" + n : n);
-  episodesArr.forEach((episode) => {
-    card.classList = "card-body";
-    // Construct card content
-    const content = `
+  return `
     <div index=${episode.id} class='card'>
     <div class='card-image'><img src=${episode.image.medium} alt=card-image ${
-      episode.name
-    }></div>
+    episode.name
+  }></div>
       <h2 class='card-title'> ${episode.name} - S${twoDigit(
-      episode.season
-    )}E${twoDigit(episode.number)} </h2>
+    episode.season
+  )}E${twoDigit(episode.number)} </h2>
       ${episode.summary}
     </div>
       `;
-    // Append newly created card element to the card
-    // console.log(card);
-    card.innerHTML += content;
-  });
-  return card;
 };
 
 const makePageForEpisodes = (episodeList) => {
-  const rootElem = document.getElementById("root");
-  rootElem.append(episodeList);
+  displayLength.innerText = `The number of displayed episodes is ${episodeList.length}`;
+  episodeList.forEach((item) => {
+    cardBody.innerHTML += createEpisodeCards(item);
+  });
 };
+
+const search = (e) => {
+  let searchQuery = e.target.value.toLowerCase();
+  let filteredArr = allEpisodes.filter(({ name, summary }) => {
+    return (
+      name.toLowerCase().includes(searchQuery) ||
+      summary.toLowerCase().includes(searchQuery)
+    );
+  });
+  cardBody.innerHTML = "";
+  makePageForEpisodes(filteredArr);
+  displayLength.innerText = `The number of displayed episodes is ${filteredArr.length}`;
+};
+
+searchInput.addEventListener("input", search);
 
 window.onload = setup;
