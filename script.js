9d22ccb1523c04eeba4c5d20f02c39853f383d0d
mahri09 <63245237+mahri09@users.noreply.github.com>
2022-02-24 23:42:11 +0000

    completed Level-300, added select input and filter and shows only one episode

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const allEpisodes = getAllEpisodes();
 const cardBody = document.getElementById("card-body");
 const searchInput = document.getElementById("search-input");
+const selectInput = document.getElementById("select-episode");
 const displayLength = document.querySelector(".display-length");
 
 function setup() {
   makePageForEpisodes(allEpisodes);
+  select(allEpisodes);
 }
 
+const twoDigit = (n) => (n < 10 ? "0" + n : n);
 const createEpisodeCards = (episode) => {
   // Create card element
-  const twoDigit = (n) => (n < 10 ? "0" + n : n);
   return `
     <div index=${episode.id} class='card'>
     <div class='card-image'><img src=${episode.image.medium} alt=card-image ${
@@ -24,7 +26,6 @@ const createEpisodeCards = (episode) => {
 };
 
 const makePageForEpisodes = (episodeList) => {
-  displayLength.innerText = `The number of displayed episodes is ${episodeList.length}`;
   episodeList.forEach((item) => {
     cardBody.innerHTML += createEpisodeCards(item);
   });
@@ -40,9 +41,39 @@ const search = (e) => {
   });
   cardBody.innerHTML = "";
   makePageForEpisodes(filteredArr);
-  displayLength.innerText = `The number of displayed episodes is ${filteredArr.length}`;
+  displayLength.innerText = `The number of displayed episodes is ${filteredArr.length}/${allEpisodes.length}`;
 };
 
 searchInput.addEventListener("input", search);
+
+const select = (episodeList) => {
+  episodeList.forEach((episode) => {
+    let option = document.createElement("option");
+    option.setAttribute(
+      "value",
+      `S${twoDigit(episode.season)}E${twoDigit(episode.number)} - ${
+        episode.name
+      }`
+    );
+    option.innerText = `S${twoDigit(episode.season)}E${twoDigit(
+      episode.number
+    )} - ${episode.name}`;
+    selectInput.appendChild(option);
+  });
+};
+
+selectInput.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  let filteredArr = allEpisodes.filter((episode) => {
+    return (
+      `S${twoDigit(episode.season)}E${twoDigit(episode.number)} - ${
+        episode.name
+      }` === e.target.value
+    );
+  });
+  console.log(filteredArr);
+  cardBody.innerHTML = "";
+  makePageForEpisodes(filteredArr);
+});
 
 window.onload = setup;
